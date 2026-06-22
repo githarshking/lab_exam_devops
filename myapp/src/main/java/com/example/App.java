747c5d983c44762a57fd58c0d5b88097b373efff
Harshit Raj <githarshkingraaj@gmail.com>
2026-06-22 14:13:10 +0530

    upload maven project

---
+++ myapp/src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int result= 2+3;
+        System.out.println( "2 +3 ="+ result );
+    }
+}
